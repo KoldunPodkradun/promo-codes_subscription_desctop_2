d0a62f6da85e84ad7d6e0a34175a3525cb9297c7
KoldunPodkradun <koldunpodkradun@gmail.com>
2020-03-18 18:24:07 +0300

    18.03.2020 18.23

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
+$('.next').on('click', function () {
+    event.preventDefault();
+    $('.subscription-step-2').show();
+    $('.subscription-step-1').hide();
+});
+
+$('.prev').on('click', function () {
+    event.preventDefault();
+    $('.subscription-step-2').hide();
+    $('.subscription-step-1').show();
+});
+
 $('.unsubscribe').on('click', function () {
-   $('.popup-wrapp').addClass('active');
+    $('.popup-wrapp, .popup-one').addClass('active');
 });
 
 $('.close-popup').on('click', function () {
     $('.popup-wrapp').removeClass('active');
 });
 
+$('.unsubscribe-two').on('click', function () {
+    $('.popup-one').removeClass('active');
+    $('.popup-two').addClass('active');
+});
