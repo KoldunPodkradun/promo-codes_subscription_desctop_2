ec797a10e1ad0e6018339a06f970f37378fa8991
KoldunPodkradun <koldunpodkradun@gmail.com>
2020-03-18 17:53:21 +0300

    18.03.2020 17.52

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$('.unsubscribe').on('click', function () {
+   $('.popup-wrapp').addClass('active');
+});
+
+$('.close-popup').on('click', function () {
+    $('.popup-wrapp').removeClass('active');
+});
+
